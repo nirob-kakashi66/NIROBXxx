1c9b7c57d1612ae3c33a89be5ba25f94e0c7b73d
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-03-26 14:58:37 +0000

    Yu

--- Goat.js
+++ Goat.js
@@ -47,7 +47,7 @@ function validJSON(pathDir) {
 }
 
 const { NODE_ENV } = process.env;
-const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.dev.json'}`);
+const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.json' : '.dev.json'}`);
 const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.dev.json'}`);
 const dirAccount = path.normalize(`${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.dev.txt' : '.dev.txt'}`);
 
